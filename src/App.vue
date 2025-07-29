30732441c4a34595ca841e955986dac32e80b21e
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-07-30 00:15:52 +0300

    feat: add basic layout and components
    - adds hiragana, katakana, jlpt components
    - adds tabs component for navigation
    - uses hash navigation for tab selection

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Hiragana from "@/components/Hiragana.vue";
+import Katakana from "@/components/Katakana.vue";
+import Jlpt from "./components/Jlpt.vue";
+import Tabs from "./components/Tabs.vue";
+import { isActive } from "@/utils";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const currentHash = ref(window.location.hash);
+function updateHash() {
+  currentHash.value = window.location.hash;
+}
+
+onMounted(() => {
+  window.addEventListener("hashchange", updateHash);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("hashchange", updateHash);
+});
+</script>
 
 <template>
-  <h1>Learning Japanese Tracker</h1>
-  <p>
-    
-  </p>
-</template>
+  <div class="space-y-10 pb-32">
+    <h1 class="text-4xl text-center font-bold pt-8">🇯🇵 Learning Tracker 🇯🇵</h1>
+    <Tabs />
 
-<style></style>
+    <Hiragana
+      v-if="isActive('#hiragana', currentHash) || isActive('', currentHash)" />
+    <Katakana v-if="isActive('#katakana', currentHash)" />
+    <Jlpt v-if="isActive('#jlpt', currentHash)" />
+  </div>
+</template>
