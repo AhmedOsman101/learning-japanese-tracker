e766ca319b002453ab0324bf64f3ba9cb75f186d
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-07-30 00:15:08 +0300

    feat: add hiragana, katakana, and jlpt components

---
+++ src/components/Hiragana.vue
@@ -0,0 +1,16 @@
+<template>
+  <PieChart
+    :data="[
+      { name: 'Memorized', value: hiragana.memorized },
+      {
+        name: 'Remaining',
+        value: hiragana.total - hiragana.memorized,
+      },
+    ]"
+    title="Hiragana" />
+</template>
+
+<script setup lang="ts">
+import PieChart from "./PieChart.vue";
+import { hiragana } from "../data/kani.json";
+</script>
---
+++ src/components/Katakana.vue
@@ -0,0 +1,16 @@
+<template>
+  <PieChart
+    :data="[
+      { name: 'Memorized', value: katakana.memorized },
+      {
+        name: 'Remaining',
+        value: katakana.total - katakana.memorized,
+      },
+    ]"
+    title="Katakana" />
+</template>
+
+<script setup lang="ts">
+import PieChart from "./PieChart.vue";
+import { katakana } from "../data/kani.json";
+</script>
