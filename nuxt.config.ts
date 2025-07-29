791a234b152af4948727d0b8d1a2bff3b4e96ec9
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-07-30 02:50:45 +0300

    chore: update format script and styling
    - update format script to allow errors
    - adjust tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
   vite: { plugins: [tailwindcss()] },
   alias: {
-    "shared": fileURLToPath(new URL("./shared", import.meta.url)),
+    shared: fileURLToPath(new URL("./shared", import.meta.url)),
     "@": fileURLToPath(new URL("./app", import.meta.url)),
     "~": fileURLToPath(new URL("./", import.meta.url)),
   },
