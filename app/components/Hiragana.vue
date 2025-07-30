abe74ba76583a840bd8a0483beaf1f83aabf635a
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-07-30 20:31:10 +0300

    feat: fetch kani data from github
    - fetch data using useFetch
    - add loading state

--- app/components/Hiragana.vue
+++ app/components/Hiragana.vue
@@ -1,21 +1,43 @@
 <template>
-  <PieChart
-    :data="[
-      { name: 'Memorized', value: hiragana.memorized },
-      {
-        name: 'Remaining',
-        value: remaining,
-      },
-    ]"
-    title="Hiragana" />
-  <Edit
-    class="w-full"
-    file="kani"
-    field="hiragana"
-    :remaining />
+  <div v-if="!pending">
+    <PieChart
+      :data="[
+        { name: 'Memorized', value: hiragana.memorized },
+        {
+          name: 'Remaining',
+          value: remaining,
+        },
+      ]"
+      title="Hiragana" />
+    <Edit
+      class="w-full"
+      file="kani"
+      field="hiragana"
+      :remaining />
+  </div>
+
+  <Loading v-else />
 </template>
 
 <script setup lang="ts">
-import { hiragana } from "~/public/data/kani.json";
-const remaining = ref(hiragana.total - hiragana.memorized);
+import { GITHUB_RAW_URL } from "~/shared/constants";
+const { data, pending } = useFetch(`${GITHUB_RAW_URL}/data/kani.json`, {
+  transform: JSON.parse,
+  lazy: true,
+});
+
+const hiragana = ref({ memorized: 0, total: 0 });
+const remaining = ref(0);
+
+watch(
+  data,
+  (val) => {
+    console.log(val)
+    if (val && val.hiragana) {
+      hiragana.value = val.hiragana;
+      remaining.value = val.hiragana.total - val.hiragana.memorized;
+    }
+  },
+  { immediate: true }
+);
 </script>
--- app/components/Katakana.vue
+++ app/components/Katakana.vue
@@ -1,22 +1,43 @@
 <template>
-  <PieChart
-    :data="[
-      { name: 'Memorized', value: katakana.memorized },
-      {
-        name: 'Remaining',
-        value: remaining,
-      },
-    ]"
-    title="Katakana" />
-  <Edit
-    class="w-full"
-    file="kani"
-    field="katakana"
-    :remaining />
+  <div v-if="!pending">
+    <PieChart
+      :data="[
+        { name: 'Memorized', value: katakana.memorized },
+        {
+          name: 'Remaining',
+          value: remaining,
+        },
+      ]"
+      title="Katakana" />
+    <Edit
+      class="w-full"
+      file="kani"
+      field="katakana"
+      :remaining />
+  </div>
+
+  <Loading v-else />
 </template>
 
 <script setup lang="ts">
-import { katakana } from "~/public/data/kani.json";
+import { GITHUB_RAW_URL } from "~/shared/constants";
 
-const remaining = ref(katakana.total - katakana.memorized);
+const { data, pending } = useFetch(`${GITHUB_RAW_URL}/data/kani.json`, {
+  transform: JSON.parse,
+  lazy: true,
+});
+
+const katakana = ref({ memorized: 0, total: 0 });
+const remaining = ref(0);
+
+watch(
+  data,
+  (val) => {
+    if (val && val.katakana) {
+      katakana.value = val.katakana;
+      remaining.value = val.katakana.total - val.katakana.memorized;
+    }
+  },
+  { immediate: true }
+);
 </script>
