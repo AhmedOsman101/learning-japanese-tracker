ecb15f59d55a6b44e04ef30d03b32b8bf1021193
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-07-30 20:30:39 +0300

    feat: Added @nuxt/mongoose module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  devtools: { enabled: false },
+  modules: ["@nuxt/ui", "nuxt-mongoose"],
   css: ["@/assets/css/main.css"],
   vite: { plugins: [tailwindcss()] },
   alias: {
